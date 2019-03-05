e9af438f424f31b3f147ab358511a7991a9f8d45
Christian Hansen <ChrisWcs@users.noreply.github.com>
2019-03-05 15:27:48 -0700

    Fix test create cipherUtils.js

--- src/utils/cipherUtils.spec.js
+++ src/utils/cipherUtils.spec.js
@@ -5,7 +5,7 @@ describe("shift function", () => {
     it("shift message 1", () => {
         const initial = "hello";
         const expected = "ifmmp";
-        const actual = shift(initial);
+        const actual = shift( 1, initial );
 
         expect(actual).toBe(expected);
     })
